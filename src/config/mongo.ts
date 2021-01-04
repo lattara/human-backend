be2eefdfe8b77c09f91cf01d5082f957d760d0a3
lattara <atopic.ie@gmail.com>
2021-01-04 17:05:44 +0100

    setting up .env

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema
 export class mongooseConnection {
     // connection to the cloud database
     static connect(callback: any){
-        const dbConnectionString = 'mongodb+srv://human:jasam4242@human.x14kg.mongodb.net/human?retryWrites=true&w=majority'
+        const dbConnectionString = process.env.MONGODBURL
         mongoose.connect(dbConnectionString, {useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify: false})
         .then((result: any)=> {
             console.log('Successfully connected to the Database')
