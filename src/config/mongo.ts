86ca78f15c0b799d777d4cf9f65fb98d43914d72
lattara <atopic.ie@gmail.com>
2021-01-04 16:57:43 +0100

    product routes finished

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -7,7 +7,7 @@ export class mongooseConnection {
     // connection to the cloud database
     static connect(callback: any){
         const dbConnectionString = 'mongodb+srv://human:jasam4242@human.x14kg.mongodb.net/human?retryWrites=true&w=majority'
-        mongoose.connect(dbConnectionString, {useNewUrlParser: true, useUnifiedTopology:true})
+        mongoose.connect(dbConnectionString, {useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify: false})
         .then((result: any)=> {
             console.log('Successfully connected to the Database')
             callback
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,7 +1,7 @@
 import express, { Router, Request, Response } from "express";
 const Product = require("../models/product.model")
-import { errorMsg } from "../errors/errormsg"
 const router: Router = express.Router();
+
 
 // GET
 
@@ -12,7 +12,7 @@ router.get("/", async (req: Request, res: Response) => {
         res.json(products)
         res.status(200)
     } catch (err) {
-        res.status(500).json(errorMsg("DatabaseError"))
+        res.status(500).send('Error connecting to the Database');
     }
 });
 
@@ -24,7 +24,7 @@ router.get("/necklaces", async (req: Request, res: Response) => {
         res.json(products)
         res.status(200)
     } catch (err) {
-        res.status(500).json(errorMsg("DatabaseError"))
+        res.status(500).send('Error connecting to the Database');
     }
 });
 
@@ -36,25 +36,21 @@ router.get("/bracelets", async (req: Request, res: Response) => {
         res.json(products)
         res.status(200)
     } catch (err) {
-        res.status(500).json(errorMsg("DatabaseError"))
+        res.status(500).send('Error connecting to the Database');
     }
 });
 
-// get specific necklace
-router.get("/necklaces/:id", getProduct, (req: any, res: any) => {
-    res.json(res.product.name)
-});
-
-// get all bracelets
-router.get("/bracelets/:id", async (req: any, res: any) => {
-    try {
-        const query = { "_id": req.params.id, "category": "bracelet" }
-        const products = await Product.find(query)
-        res.json(products)
-        res.status(200)
-    } catch (err) {
-        res.status(500).json(errorMsg("DatabaseError"))
-    }
+// get specific product
+router.get("/products/:id", async (req: any, res: any) => {
+    Product.findById(req.params.id)
+    .then((result: any)=> {
+        if (!result){
+            res.status(404).send('Product not found, please try again')
+        }else {
+            res.status(200).json(result)
+            }    
+    })
+    .catch((err: any)=> res.status(500).json({message: 'Error connecting to the Database'}))
 });
 
 // POST
@@ -70,18 +66,41 @@ router.post("/createProduct", async (req: Request, res: Response) => {
         isSold: req.body.isSold,
         tags: req.body.tags
     })
-    newProduct.save((err: any, resp: Response) => {
+    newProduct.save((err: Error, resp: Response) => {
         if (err) {
-            res.send({message: err.message})
+            res.status(500).send('Error connecting to the Database');
         } else {
-            res.send(newProduct)
+            res.status(200).json(newProduct)
         }
     })
-
-// DELETE PRODUCT
-
-
 })
 
-// middleware 
+//PATCH
+
+//Update product
+router.patch("/updateProduct/:id", async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
+        const productToUpdate = req.body
+        const updatedProduct = await Product.findByIdAndUpdate(id, {$set: productToUpdate}, {new: true})
+        res.status(200).json(updatedProduct)
+    } catch (error) {
+        res.status(500).send('Error occurred while updating the project. Please try again.');
+    }
+})
+
+// DELETE
+
+// Delete product
+router.delete("/deleteProduct/:id", async(req: Request, res: Response) => {
+    Product.findByIdAndDelete(req.params.id)
+    .then(()=>{
+        res.status(200).send('Product deleted successfully')
+    })
+    .catch((err: any)=>{
+        res.status(500).send('Error in deleting product, please try again later')
+    })
+ })
+
+
 module.exports = router
